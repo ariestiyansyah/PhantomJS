3fa8f44b2c0107cf99c87696376b5eb37cad03b0
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-07-11 12:43:46 +0700

    IndonesiaX Status Testing

---
+++ status.js
@@ -0,0 +1,10 @@
+var page = require('webpage').create();
+page.open('https://indonesiax.co.id', function (status) {
+        if (status != 'success') {
+            console.log('IndonesiaX is down');
+        }
+        else {
+            console.log('IndonesiaX is up');
+        }
+        phantom.exit();
+});
